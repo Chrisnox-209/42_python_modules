a565065646601ab770fdc2c8ae217cc33673e8c4
Chrisnox-209 <89594242+Chrisnox-209@users.noreply.github.com>
2026-02-23 10:05:01 +0100

    refactor(python1/ex6): Organisation du code et ajout des types hints

--- Python_01/ex6/ft_garden_analytics.py
+++ Python_01/ex6/ft_garden_analytics.py
@@ -1,20 +1,8 @@
-class GardenManager:
-    def __init__(self) -> None:
-        self.gardens = {}
-
-    def add_plant(self, garden_name: str, plant: Plant) -> None:
-        if garden_name in self.gardens:
-            self.gardens[garden_name] = self.gardens[garden_name] + [plant]
-            print(f"Added {plant.name} to {self.owner_name}'s Garden")
-        else:
-            self.gardens[garden_name] = [plant]
-            print(f"Added {plant.name} to {self.owner_name}'s Garden")
-
 class Plant:
     def __init__(self, name_plant: str, height: int, age: int) -> None:
-        self.name_plant = name_plant
-        self.height = height
-        self.age = age
+        self.name_plant: str = name_plant
+        self.height: int = height
+        self.age: int = age
 
     def grow(self) -> None:
         print(f"{self.name_plant} grew 1cm")
@@ -23,22 +11,38 @@ class Plant:
     def get_info(self) -> str:
         return f"- {self.name_plant}: {self.height}cm, {self.age} days."
 
+
 class FloweringPlant(Plant):
-    def __init__(self, name_plant: str, height: int, age: int, color: str) -> None:
+    def __init__(self, name_plant: str, height: int,
+                 age: int, color: str) -> None:
         super().__init__(name_plant, height, age)
-        self.color = color
+        self.color: str = color
 
     def get_info(self) -> str:
         return
+
 
 class PrizeFlower(FloweringPlant):
     def __init__(self, name_plant: str, height: int, age: int, color: str,
                  prize: int) -> None:
         super().__init__(name_plant, height, age, color)
-        self.prize = prize
+        self.prize: int = prize
 
     def get_info(self) -> str:
         return
+
+
+class GardenManager:
+    def __init__(self) -> None:
+        self.gardens: dict = {}
+
+    def add_plant(self, garden_name: str, plant: Plant) -> None:
+        if garden_name in self.gardens:
+            self.gardens[garden_name] = self.gardens[garden_name] + [plant]
+            print(f"Added {plant.name} to {self.owner_name}'s Garden")
+        else:
+            self.gardens[garden_name] = [plant]
+            print(f"Added {plant.name} to {self.owner_name}'s Garden")
 
 
 if __name__ == "__main__":
