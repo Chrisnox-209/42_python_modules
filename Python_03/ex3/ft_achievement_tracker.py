664127cfd43c4b3ee01f18ca7da1379515f7ab1d
Chrisnox-209 <89594242+Chrisnox-209@users.noreply.github.com>
2026-03-03 16:22:00 +0100

    ➕ add(python3/ex3): Ajout de l'exercice 3 module 3

--- Python_03/ex3/ft_achievement_tracker.py
+++ Python_03/ex3/ft_achievement_tracker.py
@@ -1,4 +1,4 @@
-def achievement_tracker() -> None:
+def achievement_data() -> None:
     alice_set: set = {
         "first_kill",
         "level_10",
@@ -18,39 +18,44 @@ def achievement_tracker() -> None:
         "boss_slayer",
         "speed_demon",
         "perfectionist"
-    }    
+    }
+
     print(f"Player alice achievements: {alice_set}")
     print(f"Player bob achievements: {bob_set}")
     print(f"Player charlie achievements: {charlie_set}")
+    print()
+    achievement_analytics(alice_set, bob_set, charlie_set)
 
 
-def achievement_analytics(alice_set, bob_set, charlie_set):
-    result = alice_set.union(bob_set.union(charlie_set))
-    print(result)
+def achievement_analytics(alice_set, bob_set, charlie_set) -> None:
+    print("=== Achievement Analytics ===")
+
+    result: set = alice_set.union(bob_set.union(charlie_set))
+    print(f"All unique achievements: {result}")
+    print(f"Total unique achievements: {len(result)}")
+    print()
+
+    result = alice_set.intersection(bob_set.intersection(charlie_set))
+    print(f"Common to all players: {result}")
+
+    unique_1: set = alice_set - (bob_set | charlie_set)
+    unique_2: set = bob_set - (alice_set | charlie_set)
+    unique_3: set = charlie_set - (alice_set | bob_set)
+    result = unique_1 | unique_2 | unique_3
+    print(f"Rare achievements (1 player): {result}")
+    print()
+
+    result = alice_set.intersection(bob_set)
+    print(f"Alice vs Bob common: {result}")
+
+    result = alice_set.difference(bob_set)
+    print(f"Alice unique: {result}")
+
+    result = bob_set.difference(alice_set)
+    print(f"Bob unique: {result}")
 
 
 if __name__ == "__main__":
-    alice_set: set = {
-        "plop",
-        "plip",
-        "plap",
-        "plep"
-    }
-    bob_set: set = {
-        "plop",
-        "plip",
-        "plap",
-        "plup"
-    }
-
-    charlie_set: set = {
-        "plop",
-        "plip",
-        "pfff",
-        "plyp"
-    }  
     print("=== Achievement Tracker System ===")
     print()
-    # achievement_tracker()
-    achievement_analytics(alice_set, bob_set, charlie_set)
-    print()
+    achievement_data()
