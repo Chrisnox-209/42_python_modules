4d0c3e0751c2c6cde8b5bf4e1a652f7e703fc523
Chrisnox-209 <89594242+Chrisnox-209@users.noreply.github.com>
2026-03-04 14:13:09 +0100

    🚧 wip(python3/ex4): Mise à jour du code. Développement en cours ...

--- Python_03/ex4/ft_inventory_system.py
+++ Python_03/ex4/ft_inventory_system.py
@@ -1,12 +1,21 @@
 import sys
 
 def ft_split(string: str):
-    word: str = ""
-    for char in string:
-        if char == ":":
-            
-        else:
-            word += char
+    dico = {}
+    cle: str = ""
+    valeur: str = ""
+    flag_cle = 0
+    end = chaine[len(chaine) - 1]
+    for lettre in chaine:
+        if lettre == ":":
+            if cle != "" and flag_cle == 0:
+                flag_cle = 1
+        elif flag_cle == 0:
+            cle += lettre
+        elif flag_cle == 1:
+                valeur += lettre
+    dico[cle] = valeur
+    return dico
 
 def parse():
     argument: list = sys.argv[1:]
@@ -15,7 +24,6 @@ def parse():
         sys.exit()
     else:
         for arg in argument:
-            
+
 
 if __name__ == "__main__":
-    
