ab3dc2a28705e067cffa1a88ae238da15e539d71
Chrisnox-209 <89594242+Chrisnox-209@users.noreply.github.com>
2026-03-11 17:08:57 +0100

    🔨 refactor(python3/ex6): modification des fonction list, dict, set et formatage en comprehension

--- Python_03/ex6/ft_analytics_dashboard.py
+++ Python_03/ex6/ft_analytics_dashboard.py
@@ -2,35 +2,26 @@ from typing import Any
 
 
 def list_comprehension(data: dict) -> None:
-    list_name: list = []
-    list_score: list = []
-    list_active: list = []
-    for name in data:
-        list_score.append(data[name]["score"] * 2)
-        if data[name]["score"] > 2000:
-            list_name.append(name)
-        if data[name]["active"]:
-            list_active.append(name)
+    list_name: list = [data[name]["score"] * 2 for name in data]
+    list_score: list = [name for name in data if data[name]["score"] > 2000]
+    list_active: list = [name for name in data if data[name]["active"]]
     print(f"High scorers (>2000): {list_name}")
     print(f"Scores doubled: {list_score}")
     print(f"Active players: {list_active}")
 
 
 def dict_comprehension(data: dict) -> None:
-    dict_scores: dict = {}
+    dict_scores: dict = {name: data[name]["score"] for name in data}
     dict_categories: dict = {}
-    dict_achievements: dict = {}
-    number_achievements: int = 0
+    dict_achievements: dict = {name: len(data[name]["achievements"])
+                               for name in data}
     for name in data:
-        dict_scores[name] = data[name]["score"]
         if data[name]["score"] < 2000:
             dict_categories["low"] = 1
         elif data[name]["score"] > 2200:
             dict_categories["high"] = 3
         else:
             dict_categories["medium"] = 2
-        number_achievements = len(data[name]["achievements"])
-        dict_achievements[name] = number_achievements
 
     """ dictionary sort """
     dict_sort: dict = {}
@@ -49,15 +40,13 @@ def dict_comprehension(data: dict) -> None:
 
 
 def set_comprehension(data: dict) -> None:
-    set_player: set = set()
-    set_achievements: set = set()
-    set_regions: set = set()
-
-    for name in data:
-        set_player.add(name)
-        set_regions.add(data[name]["region"])
-        for achievements in data[name]["achievements"]:
-            set_achievements.add(achievements)
+    set_player: set = {name for name in data}
+    set_regions: set = {v["region"] for v in data.values()}
+    set_achievements: set = {
+        achievement
+        for v in data.values()
+        for achievement in v["achievements"]
+    }
     print(f"Unique players: {sorted(set_player)}")
     print(f"Unique achievements: {set_achievements}")
     print(f"Active regions: {sorted(set_regions, reverse=True)}")
