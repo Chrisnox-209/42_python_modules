0379d393acf95c3cf820e741b5226cee5919fe1d
Chrisnox-209 <89594242+Chrisnox-209@users.noreply.github.com>
2026-03-23 13:09:24 +0100

    🚧 wip:(python7/ex1): mise à jour | Travail en cours ...

--- Python_07/ex1/Deck.py
+++ Python_07/ex1/Deck.py
@@ -1,11 +1,13 @@
+from ex0.Card import Card
+from random import shuffle
 
 class Deck:
-    def __init__(self, card: Card):
-        self.card = []
+    def __init__(self):
+        self.list_card = []
 
     def add_card(self, card: Card) -> None:
-        card.append(self.card)
-        print(f"{self.name} la carte a ete ajoute")
+        self.list_card.append(card)
+        print(f"{card.name} la carte a ete ajoute")
 
     def remove_card(self, card_name: str) -> bool:
         for name in self.card:
@@ -14,8 +16,31 @@ class Deck:
                 return True
         return False
 
-def shuffle(self) -> None
-def draw_card(self) -> Card
-def get_deck_stats(self) -> dict
+    def shuffle(self) -> None:
+        random.shuffle(self.card)
+
+    # def draw_card(self) -> Card:
+
+    def get_deck_stats(self) -> dict:
+        creatures: int = 0
+        spells: int = 0
+        artifacts: int = 0
+        total_cost: int = 0
+
+        for monstre in self.list_card:
+            if monstre.type == "Creature":
+                creatures += 1
+            if monstre.type == "spell":
+                spells += 1
+            if monstre.type == "artifact":
+                artifacts += 1
+            total_cost += monstre.cost
+        size_dict = len(self.list_card)
+
+        return {"total_cards": size_dict,
+                "creatures": creatures,
+                "spells": spells,
+                "artifacts": artifacts,
+                "total_cost": total_cost/size_dict }
 
 
--- Python_07/ex1/main.py
+++ Python_07/ex1/main.py
@@ -1,2 +1,26 @@
-from ex1.CreatureCard import CreatureCard
+from ex0.CreatureCard import CreatureCard
+from ex1.Deck import Deck
 import sys
+
+
+if __name__ == "__main__":
+	print("=== card creation ===\n")
+	print(" -----------------------------------------------------"
+	      "------------------------------------------\n"
+	      "|     name     |     type     |     cost     |     rarity     "
+		  "|     attack     |     health     |\n"
+		  " -----------------------------------------------------"
+	      "------------------------------------------")
+	try:
+		card_01 = CreatureCard("Fire Dragon", 5, "Legendary", 7, 5)
+		print(f"|  {card_01.name} |   {card_01.type}   |       {card_01.cost}      |   {card_01.Rarity_Enum}    |       {card_01.attack}        |       {card_01.health}        |")
+	except ValueError as error:
+		print(error)
+		sys.exit(1)
+	finally:
+		print()
+
+	deck_obj = Deck()
+	deck_obj.add_card(card_01)
+
+	print(deck_obj.get_deck_stats())
