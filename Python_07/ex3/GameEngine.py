4e36a5bcdbf976cbc90d484d59bfc47ddd1cb150
Chrisnox-209 <89594242+Chrisnox-209@users.noreply.github.com>
2026-04-01 15:33:11 +0200

    🔨 refactor(python7/ex3):

--- Python_07/ex3/GameEngine.py
+++ Python_07/ex3/GameEngine.py
@@ -1,16 +1,14 @@
 from ex3.CardFactory import CardFactory
 from ex3.GameStrategy import GameStrategy
-import random
+from typing import Any
 import sys
 
 
 class GameEngine:
-    def __init__(self, hand_list: list, battlefield_list: list) -> None:
-        self.hand_list: list = hand_list
-        self.battlefield_list: list = battlefield_list
+    def __init__(self) -> None:
         self.turns: int = 0
-        self.total_damage: int = random.randint(1, 50)
-        self.cards_created: int = len(self.hand_list)
+        self.total_damage: int = 0
+        self.cards_created: int = 0
 
     def configure_engine(self, factory: CardFactory,
                          strategy: GameStrategy) -> None:
@@ -18,13 +16,39 @@ class GameEngine:
         self.strategy: GameStrategy = strategy
 
     def simulate_turn(self) -> dict:
-        sys.stdout.write("hand: [")
-        for i, card in enumerate(self.hand_list):
+        hand_list: list = []
+        battlefield_list: list = []
+        try:
+            my_deck_dict: dict = self.factory.create_themed_deck(3)
+        except Exception as error:
+            print(f"Deck themed creation error: {error}")
+            sys.exit(1)
+        for value in my_deck_dict.values():
+            hand_list.extend(value)
+
+        self.cards_created += len(hand_list)
+
+        try:
+            enemy: dict = self.factory.create_ennemy_deck(5)
+        except Exception as error:
+            print(f"Deck ennemy creation error: {error}")
+            sys.exit(1)
+
+        for value in enemy.values():
+            battlefield_list.extend(value)
+
+        sys.stdout.write("Hand: [")
+        for i in range(len(hand_list)):
+            card: Any = hand_list[i]
             sys.stdout.write(f"{card.name} ({card.cost})")
-            if i != len(self.hand_list) - 1:
+            if i != len(hand_list) - 1:
                 sys.stdout.write(", ")
-        sys.stdout.write("]\n")
-        return {"Cards": self.hand_list}
+        sys.stdout.write("]")
+
+        resultats_du_tour: dict = self.strategy.execute_turn(hand_list,
+                                                             battlefield_list)
+        self.turns += 1
+        return resultats_du_tour
 
     def get_engine_status(self) -> dict:
         return {
