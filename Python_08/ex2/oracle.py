bc9589da5b6b4dbfd9ef7fcadf255c139a356c7d
Chrisnox-209 <89594242+Chrisnox-209@users.noreply.github.com>
2026-04-06 22:50:47 +0200

    🧩 feat(python8/ex2): ajout de la fonction check_data & refactorisation du code + norme pep8 et types hints

--- Python_08/ex2/oracle.py
+++ Python_08/ex2/oracle.py
@@ -1,24 +1,59 @@
 from dotenv import load_dotenv
 import os
-from typing import Any
+
+
+def check_data() -> bool:
+    check = True
+    if os.getenv("MATRIX_MODE"):
+        print(f"Mode: {os.getenv("MATRIX_MODE")}")
+    else:
+        print("[ERROR]: MATRIX_MODE no value")
+        check = False
+
+    if os.getenv("DATABASE_URL"):
+        print(f"Database: {os.getenv("DATABASE_URL")}")
+    else:
+        print("[ERROR]: DATABASE_URL no value")
+        check = False
+
+    if os.getenv("API_KEY"):
+        print(f"API Access: {os.getenv("API_KEY")}")
+    else:
+        print("[ERROR]: API_KEY no value")
+        check = False
+
+    if os.getenv("LOG_LEVEL"):
+        print(f"Log Level: {os.getenv("LOG_LEVEL")}")
+    else:
+        print("[ERROR]: LOG_LEVEL no value")
+        check = False
+
+    if os.getenv("ZION_ENDPOINT"):
+        print(f"Zion Network: {os.getenv("ZION_ENDPOINT")}")
+    else:
+        print("[ERROR]: ZION_ENDPOINT no value")
+        check = False
+    print()
+    return check
 
 
 if __name__ == "__main__":
+    print("\nORACLE STATUS: Reading the Matrix...\n")
+
+    env_loaded: bool = load_dotenv()
+    data_ok: bool = check_data()
+
+    print("Environment security check:\n"
+          "[OK] No hardcoded secrets detected")
 
     if load_dotenv() is not True:
-        print("ERROR: .env file found")
+        print("[ERROR]: .env file found")
+    else:
+        print("[OK] .env file properly configured")
 
-    matrix_Mode: Any = os.getenv("MATRIX_MODE")
-    database_url: Any = os.getenv("DATABASE_URL")
-    api_key: Any = os.getenv("API_KEY")
-    log_level: Any = os.getenv("ZION_ENDPOIN")
+    if data_ok:
+        print("[OK] Production overrides available")
+    else:
+        print("[ERROR]: Production options are incorrect")
 
-    list_config: list = [matrix_Mode,
-                         database_url,
-                         api_key,
-                         log_level]
-
-    if not list_config:
-        print("ERROR: no value, edit .env with your values")
-        
-
+    print("\nThe Oracle sees all configurations.")
